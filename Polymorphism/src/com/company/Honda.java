93b7ca319ca97e5cd3b837f7a195a342949feae2
Raj <raj@evocalize.com>
2018-09-19 14:53:22 -0700

    added Polymorphism code

--- Polymorphism/src/com/company/Honda.java
+++ Polymorphism/src/com/company/Honda.java
@@ -1,39 +1,5 @@
 package com.company;
 
-class Car {
-    private boolean engine;
-    private int cylinders;
-    private String name;
-    private int wheels;
-
-    public Car(int cylinders, String name) {
-        this.cylinders = cylinders;
-        this.name = name;
-        this.wheels = 4;
-        this.engine = true;
-    }
-
-    public int getCylinders() {
-        return cylinders;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String startEngine(){
-        return "Car -> starting engine";
-    }
-
-    public String accelerate(){
-        return "Car -> accelerating";
-    }
-
-    public String brake(){
-        return "Car -> brakes";
-    }
-}
-
 class Honda extends Car{
     public Honda(int cylinders, String name) {
         super(cylinders, name);
@@ -56,18 +22,3 @@ class Honda extends Car{
 }
 
 
-public class Main {
-
-    public static void main(String[] args) {
-	// write your code here
-        Car car = new Car(9, "Base");
-        System.out.println(car.startEngine());
-        System.out.println(car.accelerate());
-        System.out.println(car.brake());
-
-        Honda h = new Honda(6, "City");
-        System.out.println(h.startEngine());
-        System.out.println(h.accelerate());
-        System.out.println(h.brake());
-    }
-}
--- Polymorphism/src/com/company/Main.java
+++ Polymorphism/src/com/company/Main.java
@@ -4,5 +4,14 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        Car car = new Car(9, "Base");
+        System.out.println(car.startEngine());
+        System.out.println(car.accelerate());
+        System.out.println(car.brake());
+
+        Honda h = new Honda(6, "City");
+        System.out.println(h.startEngine());
+        System.out.println(h.accelerate());
+        System.out.println(h.brake());
     }
 }
